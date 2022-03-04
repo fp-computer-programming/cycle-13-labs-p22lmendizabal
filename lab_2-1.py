818b32724a5159595211d42dbcf8d3f4e702b09e
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-03-04 10:20:17 -0500

    add files

---
+++ lab_2-1.py
@@ -0,0 +1,9 @@
+# author: LM (AMDG) 3/4/22
+def build_car():
+    wheels = 4
+    axels = 2
+    doors = 4
+    color = "red"
+    print("Your car has {0} wheels {1} axels {2} doors and is the color {3}.".format(wheels, axels, doors, color))
+
+print(build_car())
---
+++ lab_2-2.py
@@ -0,0 +1,10 @@
+#author (AMDG) 3/4/22
+
+wheels = int(input("Enter the number of wheels: "))
+axel = int(input("enter the number of axels: "))
+doors= int(input("Enter the number of doors: "))
+color= input("Enter the color of the car: ")
+
+def build_vehicle(wheels, axel, doors, color):
+    print("Your car has {0} wheels {1} axels {2} doors and is the color {3}.".format(wheels, axel, doors, color))
+build_vehicle(wheels, axel, doors, color)
